dc8e3ae6449ed51360e4ee4719b21337aa37a55e
Demiga-g <midegageorge2@gmail.com>
2024-07-23 07:22:43 +0300

    packages, environment, and configuration setup or adjustments

--- deployment_file_entry/src/mlops/project_libraries.py
+++ deployment_file_entry/src/mlops/project_libraries.py
@@ -9,10 +9,9 @@ from fastapi import FastAPI
 import uvicorn
 import numpy as np
 import pandas as pd
-import seaborn as sns
-from prefect import task, flow, get_run_logger
-from hyperopt import STATUS_OK, Trials, fmin, hp, tpe
-from hyperopt.pyll import scope
+# from prefect import task, flow, get_run_logger
+# from hyperopt import STATUS_OK, Trials, fmin, hp, tpe
+# from hyperopt.pyll import scope
 from sklearn.compose import ColumnTransformer
 from sklearn.ensemble import GradientBoostingClassifier, RandomForestClassifier
 from sklearn.feature_extraction import DictVectorizer
@@ -23,13 +22,12 @@ from sklearn.metrics import (accuracy_score, f1_score, precision_score,
 from sklearn.pipeline import Pipeline, make_pipeline
 from sklearn.preprocessing import PowerTransformer
 from sklearn.svm import SVC
-from xgboost import XGBClassifier
+# from xgboost import XGBClassifier
 
 __all__ = [
-    'argparse', 'os', 'mlflow', 'pickle', 'np', 'pd', 'sns', 'plt', 'datetime', 'task', 'flow',
-    'get_run_logger', 'fmin', 'tpe', 'hp', 'STATUS_OK', 'Trials', 'scope', 'Pipeline', 
-    'make_pipeline','SimpleImputer', 'ColumnTransformer', 'PowerTransformer', 'DictVectorizer', 
-    'XGBClassifier','GradientBoostingClassifier', 'RandomForestClassifier', 'SVC', 
+    'argparse', 'os', 'mlflow', 'pickle', 'np', 'pd', 'plt', 'datetime', 'Pipeline', 
+    'make_pipeline','SimpleImputer', 'ColumnTransformer', 'PowerTransformer', 'DictVectorizer',
+    'GradientBoostingClassifier', 'RandomForestClassifier', 'SVC', 
     'LogisticRegression', 'f1_score','recall_score', 'roc_auc_score', 'accuracy_score', 
     'precision_score', 'FastAPI', 'uvicorn'
 ]
